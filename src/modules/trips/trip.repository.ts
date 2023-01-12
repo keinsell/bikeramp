30da491e63cb505c4984ab3bd1d3b46c12669a7b
http:jajek <keinsell@protonmail.com>
2023-01-12 14:25:30 +0100

    feat: add `TripRepository`

---
+++ src/modules/trips/trip.repository.ts
@@ -0,0 +1,11 @@
+import { PrismaService } from 'src/infrastructure/prisma/prisma.service'
+import { Trip } from './trip.entity'
+import { Repository } from 'src/common/persistance/repository'
+import { TripMapper } from './trip.mapper'
+
+export class TripRepository implements Repository<Trip> {
+  constructor(private prismaService: PrismaService, private tripMapper: TripMapper) {}
+  save(entity: Trip): Promise<Trip> {
+    throw new Error('Method not implemented.')
+  }
+}
