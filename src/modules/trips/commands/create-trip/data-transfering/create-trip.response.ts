7564ef42c1e9fb15ecb8e10a6fcba3e5d4973b70
http:jajek <keinsell@protonmail.com>
2023-01-12 16:37:40 +0100

    refactor: update examples of `CreateTripResponse`

--- src/modules/trips/commands/create-trip/data-transfering/create-trip.response.ts
+++ src/modules/trips/commands/create-trip/data-transfering/create-trip.response.ts
@@ -3,32 +3,37 @@ import { ApiProperty } from '@nestjs/swagger'
 export class CreateTripResponse {
   @ApiProperty({
     description: 'Trip ID',
-    example: 'cl1234',
+    example: 'clct8aeyk0006pmrmp8b3ixro',
   })
   id: string
+
   @ApiProperty({
-    description: 'Date in format `2020-12-31`',
-    example: '2020-12-31',
+    description: 'Date in ISO format',
+    example: '2020-12-31T00:00:00.000Z',
   })
   date: Date
+
   @ApiProperty({
     description: 'Starting address in format `Plac Europejski 2, Warszawa, Polska`',
     example: 'Plac Europejski 2, Warszawa, Polska',
   })
   startAddress: string
+
   @ApiProperty({
     description: 'Destination address in format `Plac Europejski 13, Warszawa, Polska`',
     example: 'Plac Europejski 13, Warszawa, Polska',
   })
   endAddress: string
+
   @ApiProperty({
     description: 'Package price in PLN',
-    example: 100,
+    example: '1000.00 PLN',
   })
   price: string
+
   @ApiProperty({
     description: 'Distance in format `1.2 km`',
-    example: '1.2 km',
+    example: '252.17 km',
   })
   distance: string
 }
