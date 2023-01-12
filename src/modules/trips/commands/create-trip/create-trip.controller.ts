afee802267593e6d69e497305f051776f7b265b4
http:jajek <keinsell@protonmail.com>
2023-01-12 14:16:53 +0100

    refactor: implement bare `CreateTripController`

--- src/modules/trips/commands/create-trip/create-trip.controller.ts
+++ src/modules/trips/commands/create-trip/create-trip.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 import { CreateTripService } from './create-trip.service'
+import { ControllerExecutor } from 'src/common/controller'
+import { CreateTripResponse } from './create-trip.response'
 
 @Controller()
-export class CreateTripController {
-  constructor(private readonly service: CreateTripService) {}
+export class CreateTripController extends ControllerExecutor {
+  constructor(private readonly service: CreateTripService) {
+    super()
+  }
 
   @Post('/api/trips')
-  getHello(): string {
-    return this.appService.getHello()
+  protected async executeImplementation(@Body() body: CreateTripService): Promise<CreateTripResponse> {
+    // I'm going to be honest, I do not know good validation practices in NestJS.
+    // So I'll do such in most "genric" idea that I have in my head.
+
+    // TODO: Validate body
+    // TODO: Execute service
+    const response = await this.service.execute(body)
+    // TODO: Return either result or error
+
+    return response
   }
 }
--- src/modules/trips/commands/create-trip/create-trip.service.ts
+++ src/modules/trips/commands/create-trip/create-trip.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common'
+import { Usecase } from 'src/common/domain/usecase/usecase'
 import { GeocodingService } from 'src/modules/geocoding/geocoding.adapter'
+import { CreateTripResponse } from './create-trip.response'
 
 @Injectable()
-export class CreateTripService {
+export class CreateTripService implements Usecase<any, CreateTripResponse> {
   constructor(private geocodingService: GeocodingService) {}
 
-  getHello(): string {
-    return 'Hello World!'
+  execute(request: any): Promise<CreateTripResponse> {
+    throw new Error('Method not implemented.')
   }
 }
