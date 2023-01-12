a4fd05bbe209c4605d66a232c7412f52a0311a3c
http:jajek <keinsell@protonmail.com>
2023-01-12 14:35:24 +0100

    refactor: remove `AppController` and `AppService`

--- src/application/app.module.ts
+++ src/application/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common'
-import { AppController } from '../modules/trips/app.controller'
-import { AppService } from '../modules/trips/app.service'
 import { PrismaService } from '../infrastructure/prisma/prisma.service'
 import { GeocodingService } from 'src/modules/geocoding/geocoding.adapter'
 import { GeocodeMapsGeocodingService } from 'src/modules/geocoding/services/geocode-maps/geocode-maps.geocoding'
@@ -8,7 +6,7 @@ import { TripModule } from 'src/modules/trips/trip.module'
 
 @Module({
   imports: [TripModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService, { provide: GeocodingService, useClass: GeocodeMapsGeocodingService }],
+  controllers: [],
+  providers: [PrismaService, { provide: GeocodingService, useClass: GeocodeMapsGeocodingService }],
 })
 export class AppModule {}
