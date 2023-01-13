4e956b6afe644ab76c9ba7821675c29c5735b686
http:jajek <keinsell@protonmail.com>
2023-01-13 08:55:05 +0100

    refactor: listen on provided `$PORT` or `3000`

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   const prismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
 
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 3000)
 }
 
 bootstrap()
