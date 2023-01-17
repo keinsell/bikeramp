938f23e894943c11aca2618563ea720674b3aa51
http:jajek <keinsell@protonmail.com>
2023-01-18 00:54:22 +0100

    test: describe `CreateTripService` (#44)

--- src/modules/trips/commands/create-trip/create-trip.spec.ts
+++ src/modules/trips/commands/create-trip/create-trip.spec.ts
@@ -6,8 +6,11 @@ import { TripMapper } from '../../trip.mapper'
 import { PrismaService } from '../../../../infrastructure/prisma/prisma.service'
 import { GeocodingService } from '../../../geocoding/geocoding.adapter'
 import { GeocodeMapsGeocodingService } from '../../../geocoding/services/geocode-maps/geocode-maps.geocoding'
-
-// TODO: Improve tests over this spec
+import { Coordinates } from '../../../geocoding/entities/coordinates'
+import { Money } from '../../entities/money'
+import { anything, instance, mock, when } from 'ts-mockito'
+import { ok } from 'neverthrow'
+import { Trip } from '../../trip.entity'
 
 describe('CreateTripController', () => {
   let appController: CreateTripController
@@ -42,7 +45,56 @@ describe('CreateTripController', () => {
 })
 
 describe('CreateTripService', () => {
-  it('....', () => {
-    expect(true).toBe(true)
+  let service: CreateTripService
+  let geocodingService: GeocodingService
+  let tripRepository: TripRepository
+
+  const serviceRequest = {
+    start_address: 'start_address',
+    destination_address: 'destination_address',
+    price: 10,
+    date: new Date(),
+  }
+
+  beforeEach(() => {
+    const mock__geocodingService = mock(GeocodeMapsGeocodingService)
+    const mock__tripRepository = mock(TripRepository)
+
+    // Mock start_address to Coordinates(1, 2)
+    when(mock__geocodingService.getCoordinates('start_address')).thenResolve(ok(new Coordinates(1, 2)))
+    // Mock destination_address to Coordinates(3, 4)
+    when(mock__geocodingService.getCoordinates('destination_address')).thenResolve(ok(new Coordinates(3, 4)))
+
+    // Mock save to return a Trip
+    when(mock__tripRepository.save(anything())).thenResolve(
+      new Trip(
+        {
+          startAddress: serviceRequest.start_address,
+          endAddress: serviceRequest.destination_address,
+          price: Money.fromFloat(serviceRequest.price),
+          date: new Date(serviceRequest.date),
+          distance: new Coordinates(1, 2).getDistanceBetweenCoordinates(new Coordinates(3, 4)),
+        },
+        '1',
+      ),
+    )
+
+    geocodingService = instance(mock__geocodingService)
+    tripRepository = instance(mock__tripRepository)
+
+    service = new CreateTripService(geocodingService, tripRepository)
+  })
+
+  it('should create a trip', async () => {
+    const response = await service.execute(serviceRequest)
+
+    expect(response._unsafeUnwrap()).toEqual({
+      id: '1',
+      startAddress: serviceRequest.start_address,
+      endAddress: serviceRequest.destination_address,
+      price: '10.00PLN',
+      date: new Date(serviceRequest.date),
+      distance: new Coordinates(1, 2).getDistanceBetweenCoordinates(new Coordinates(3, 4)).toString(),
+    })
   })
 })
